1b5b0409321cd46abd136c64d0e1a7a015dce76c
Shubham kumar <97464092+ShubhamWP07@users.noreply.github.com>
2023-06-18 22:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+function mySqrt(x) {
+  if (x === 0 || x === 1) {
+    return x;
+  }
+
+  let left = 0;
+  let right = x;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return right;
+}
